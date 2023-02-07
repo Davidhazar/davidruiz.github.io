20bc98bff0ccd2696111432ec46ed69e75b5c3c6
David Ruiz <Achucarrodavid@gmai.com>
2023-02-07 12:19:06 -0300

    retoques finales en contacto y seccion experiencia

--- JS/Function.js
+++
@@ -1,4 +0,0 @@
-const persona = '{"nombre":"Eden","apellido":"Hazard","edad":"30","nacionalidad":"Argentino"}';
-const obj = JSON.parse(persona);
-
-document.getElementById("demo").innerHTML = "nombre "+ obj.nombre+ " apellido "+ obj.apellido;
